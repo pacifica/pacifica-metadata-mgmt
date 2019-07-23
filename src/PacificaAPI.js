39c41b7ee5bf4463458bd82981d16fe0e97991c4
David Brown <dmlb2000@gmail.com>
2019-07-23 14:48:10 -0700

    More eslint fixes
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- src/PacificaAPI.js
+++ src/PacificaAPI.js
@@ -1,41 +1,41 @@
-import React from "react";
-import Axios from "axios";
-import Grid from "@material-ui/core/Grid";
-import Checkbox from "@material-ui/core/Checkbox";
-import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from "@material-ui/icons/Delete";
-import EditIcon from "@material-ui/icons/Edit";
-import DateTimeDisplay from "./DateTime";
-import SimpleModal from "./Modal";
+import React from 'react'
+import Axios from 'axios'
+import Grid from '@material-ui/core/Grid'
+import Checkbox from '@material-ui/core/Checkbox'
+import IconButton from '@material-ui/core/IconButton'
+import DeleteIcon from '@material-ui/icons/Delete'
+import EditIcon from '@material-ui/icons/Edit'
+import DateTimeDisplay from './DateTime'
+import SimpleModal from './Modal'
 
 export const getObjectList = md_url => {
-  return Axios.get(`${md_url}/objectinfo/list`);
-};
+  return Axios.get(`${md_url}/objectinfo/list`)
+}
 
 export const filtered_to_where_args = (field_list, filtered) => {
-  let where_args = {};
+  let where_args = {}
   for (let i = 0; i < filtered.length; i++) {
-    if (filtered[i].id === "_id") {
-      where_args._id = filtered[i].value;
-      return where_args;
+    if (filtered[i].id === '_id') {
+      where_args._id = filtered[i].value
+      return where_args
     }
-    switch(field_list[filtered[i].id]) {
+    switch (field_list[filtered[i].id]) {
       case 'TEXT':
       case 'VARCHAR':
-        filtered[i].disableLike = false;
-        break;
+        filtered[i].disableLike = false
+        break
       default:
-        filtered[i].disableLike = true;
-        break;
+        filtered[i].disableLike = true
+        break
     }
-    where_args[filtered[i].id] = filtered[i].value;
+    where_args[filtered[i].id] = filtered[i].value
     if (!filtered[i].disableLike) {
-      where_args[`${filtered[i].id}_operator`] = "like";
-      where_args[filtered[i].id] = `%${filtered[i].value}%`;
+      where_args[`${filtered[i].id}_operator`] = 'like'
+      where_args[filtered[i].id] = `%${filtered[i].value}%`
     }
   }
-  return where_args;
-};
+  return where_args
+}
 
 export const convert_columns = (
   md_url,
@@ -45,22 +45,22 @@ export const convert_columns = (
   primary_keys,
   updateFunc
 ) => {
-  let field_list_copy = field_list.slice();
-  field_list_copy.unshift("Edit");
+  let field_list_copy = field_list.slice()
+  field_list_copy.unshift('Edit')
   return field_list_copy.map((key, index) => {
-    let col_def = { Header: key, accessor: key };
+    let col_def = { Header: key, accessor: key }
     switch (key) {
-      case "Edit":
+      case 'Edit':
         col_def.Cell = row => {
-          let delete_args = { force: "True" };
+          let delete_args = { force: 'True' }
           primary_keys.map((key, index) => {
-            if (key === "id") {
-              delete_args[`_${key}`] = row.row[`_${key}`];
+            if (key === 'id') {
+              delete_args[`_${key}`] = row.row[`_${key}`]
             } else {
-              delete_args[key] = row.row[key];
+              delete_args[key] = row.row[key]
             }
-            return "";
-          });
+            return ''
+          })
           return (
             <Grid container spacing={8}>
               <Grid item xs={8} sm={4}>
@@ -82,47 +82,47 @@ export const convert_columns = (
                       params: delete_args
                     })
                       .then(res => {
-                        console.log(res);
-                        updateFunc();
+                        console.log(res)
+                        updateFunc()
                       })
                       .catch(res => {
-                        console.log(JSON.stringify(res, null, 2));
-                        alert(res.response.data.traceback);
-                      });
+                        console.log(JSON.stringify(res, null, 2))
+                        alert(res.response.data.traceback)
+                      })
                   }}
                 >
                   <DeleteIcon />
                 </IconButton>
               </Grid>
             </Grid>
-          );
-        };
-        col_def.filterable = false;
-        break;
-      case "id":
-        col_def.Header = "ID";
-        col_def.accessor = "_id";
-        break;
+          )
+        }
+        col_def.filterable = false
+        break
+      case 'id':
+        col_def.Header = 'ID'
+        col_def.accessor = '_id'
+        break
       default:
-        break;
+        break
     }
     switch (field_types[key]) {
-      case "DATETIME":
+      case 'DATETIME':
         col_def.Cell = row => (
           <DateTimeDisplay key={key} defValue={row.row[key]} />
-        );
-        break;
-      case "BOOL":
+        )
+        break
+      case 'BOOL':
         col_def.Cell = row => (
           <Checkbox label={key} value={key} checked={row.row[key]} />
-        );
-        break;
+        )
+        break
       default:
-        break;
+        break
     }
-    return col_def;
-  });
-};
+    return col_def
+  })
+}
 
 export const getData = (
   md_url,
@@ -133,34 +133,28 @@ export const getData = (
   updateFunc
 ) => {
   return new Promise((resolve, reject) => {
-    Axios.get(`${md_url}/objectinfo/${object}`)
-      .then(res => {
-        let where_args = filtered_to_where_args(res.data.field_list, filtered);
-        Axios.get(`${md_url}/objectinfo/${object}`, { params: where_args })
-          .then(res => {
-            let record_count = res.data.record_count;
-            let columns = convert_columns(
-              md_url,
-              object,
-              res.data.field_list,
-              res.data.field_types,
-              res.data.primary_keys,
-              updateFunc
-            );
-            where_args.items_per_page = pageSize;
-            where_args.page_number = pageNum + 1;
-            Axios.get(`${md_url}/${object}`, { params: where_args })
-              .then(res => {
-                resolve({
-                  numPages: Math.ceil(record_count / pageSize),
-                  columns: columns,
-                  obj_list: res.data
-                });
-              })
-              .catch(reject);
+    Axios.get(`${md_url}/objectinfo/${object}`).then(res => {
+      let where_args = filtered_to_where_args(res.data.field_list, filtered)
+      Axios.get(`${md_url}/objectinfo/${object}`, { params: where_args }).then(res => {
+        let record_count = res.data.record_count
+        let columns = convert_columns(
+          md_url,
+          object,
+          res.data.field_list,
+          res.data.field_types,
+          res.data.primary_keys,
+          updateFunc
+        )
+        where_args.items_per_page = pageSize
+        where_args.page_number = pageNum + 1
+        Axios.get(`${md_url}/${object}`, { params: where_args }).then(res => {
+          resolve({
+            numPages: Math.ceil(record_count / pageSize),
+            columns: columns,
+            obj_list: res.data
           })
-          .catch(reject);
-      })
-      .catch(reject);
-  });
-};
+        }).catch(reject)
+      }).catch(reject)
+    }).catch(reject)
+  })
+}
--- src/PacificaAPI.test.js
+++ src/PacificaAPI.test.js
@@ -1,23 +1,23 @@
 /**
  * @jest-environment node
  */
-import { getObjectList, getData } from "./PacificaAPI";
+import { getObjectList, getData } from './PacificaAPI';
 
-it("gets an object list", () => {
-  expect.assertions(1);
-  return getObjectList("http://localhost:8121").then(res => {
-    expect(res.data.available_objects.relationships).toEqual("Relationships");
+it('gets an object list', () => {
+  expect.assertions(1)
+  return getObjectList('http://localhost:8121').then(res => {
+    expect(res.data.available_objects.relationships).toEqual('Relationships')
   }).catch(res => {
-    console.log(res);
-  });
-});
+    console.log(res)
+  })
+})
 
-it("gets an objects data", () => {
-  expect.assertions(2);
-  return getData("http://localhost:8121", "users", [], 1, 0).then(res => {
-    expect(res.numPages).toEqual(2);
-    expect(res.obj_list[0].network_id).toEqual("dmlb2001");
+it('gets an objects data', () => {
+  expect.assertions(2)
+  return getData('http://localhost:8121', 'users', [], 1, 0).then(res => {
+    expect(res.numPages).toEqual(2)
+    expect(res.obj_list[0].network_id).toEqual('dmlb2001')
   }).catch(res => {
-    console.log(res);
-  });
-});
+    console.log(res)
+  })
+})
