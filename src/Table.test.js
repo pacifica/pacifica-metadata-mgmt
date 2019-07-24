b832e0e691d52ccda80e614d8789ec4dfb37580e
David Brown <dmlb2000@gmail.com>
2019-07-24 10:35:57 -0700

    Fix eslint warnings.
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- src/Table.test.js
+++ src/Table.test.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom'
 import DynamicTable from './Table'
 import { act, wait } from 'react-testing-library'
 
-it('renders without crashing', async () => {
-  expect.assertions(1)
-  const div = document.createElement('div')
-  act(() => {
-    ReactDOM.render(<DynamicTable MDUrl="http://localhost:8121" object="users" />, div)
+describe("<DynamicTable /> renders", function() {
+  it('renders without crashing', async function () {
+    expect.assertions(1)
+    const div = document.createElement('div')
+    act(() => {
+      ReactDOM.render(<DynamicTable MDUrl="http://localhost:8121" object="users" />, div)
+    })
+    await wait(() => {
+      expect(div.querySelector('div.rt-resizable-header-content').textContent).toEqual('ID')
+    })
+    ReactDOM.unmountComponentAtNode(div)
   })
-  await wait(() => {
-    expect(div.querySelector('div.rt-resizable-header-content').textContent).toEqual('ID')
-  })
-  ReactDOM.unmountComponentAtNode(div)
 })
