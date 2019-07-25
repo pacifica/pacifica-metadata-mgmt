c789282bb2a290c0b480c7db1a3827178bfc78eb
David Brown <dmlb2000@gmail.com>
2019-07-24 18:16:18 -0700

    Add some release config
    Signed-off-by: David Brown <dmlb2000@gmail.com>

---
+++ src/Error.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ErrorBoundry from './Error'
+import { act } from '@testing-library/react'
+
+describe('<ErrorBoundry />', function() {
+  it('renders without crashing', function() {
+    const div = document.createElement('div')
+    act(() => {
+      ReactDOM.render(<ErrorBoundry key='something-unique' />, div)
+    })
+    ReactDOM.unmountComponentAtNode(div)
+  })
+})
