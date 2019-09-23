2ed7ad7c809f51899c394fc3700a874372529166
David Brown <dmlb2000@gmail.com>
2019-09-23 00:07:09 -0700

    Fix #26 Verify Project Instrument filters (#30)
    This test verifies that project instrument table can filter on the
    project field.
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

---
+++ tests/issue_26.test.js
@@ -0,0 +1,43 @@
+/* eslint-disable max-statements, max-len */
+const pauseTime = 100
+const saveTime = 500
+module.exports = {
+  'Search Project in Project Instrument' (browser) {
+    const runtimeBrowser = browser.capabilities.browserName.toUpperCase()
+    // Open up the browser to the website
+    browser.url('http://localhost:8080')
+    browser.pause(pauseTime)
+    // Wait for the open drawer icon to show up
+    browser.waitForElementPresent('button[id=header-open-drawer]')
+    // Open the drawer
+    browser.click('button[id=header-open-drawer]')
+    browser.pause(pauseTime)
+    // Wait for Relationships to show up in the drawer
+    browser.waitForElementPresent('div[id=listitem-project-instrument]')
+    // Click on the Relationships text
+    browser.getLocationInView('div[id=listitem-project-instrument]')
+    browser.click('div[id=listitem-project-instrument]')
+    browser.pause(pauseTime)
+    // Wait for the close drawer icon to show up
+    browser.waitForElementPresent('button[id=drawer-close-drawer]')
+    // Close the drawer
+    browser.click('button[id=drawer-close-drawer]')
+    browser.pause(pauseTime)
+    // Verify the project 1236 is in the table
+    browser.expect.element('#root').text.to.contain('1236')
+    // Wait for the `project` filter field to show up
+    browser.waitForElementPresent('#root > div > main > div:nth-child(2) > div.ReactTable > div.rt-table > div.rt-thead.-filters > div > div:nth-child(6) > input[type=text]')
+    // Enter a short string in the filter field for `project`
+    browser.pause(pauseTime)
+    browser.sendKeys('#root > div > main > div:nth-child(2) > div.ReactTable > div.rt-table > div.rt-thead.-filters > div > div:nth-child(6) > input[type=text]', '1234')
+    browser.pause(saveTime)
+    // Verify the project 1234a is in the table
+    browser.expect.element('#root').text.to.contain('1234a')
+    // Verify the project 1236 is not in the table
+    if (runtimeBrowser === 'CHROME') {
+      browser.expect.element('#root').text.to.not.contain('1236')
+    }
+    browser.pause(saveTime)
+    browser.end()
+  }
+}
